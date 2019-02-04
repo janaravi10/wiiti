cbe6e6d8abb5230f549bf685483cdc9dc3d741a0
janaravi10 <janaravi962@gmail.com>
2019-02-04 07:43:35 +0530

    Initialised the node app

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("hello world jana");
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
