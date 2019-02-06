a5d478943abb73e825a121ccf5ae09377108b300
janaravi10 <janaravi962@gmail.com>
2019-02-06 08:20:15 +0530

    added Link tag from react-router-dom

--- client/src/components/Wiiti.js
+++ client/src/components/Wiiti.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import "../css/wiiti.css";
-import logo from "../img/wiiti-image.png";
+import logo from "../img/wiiti-image.jpg";
+import { Link } from "react-router-dom";
 class Wiiti extends Component {
     render() {
         return (
             <div className="wiiti">
+                <Link to="" className="wiiti-link">
                 <img src={logo} alt="" className="query-image" />
                 <p className="subtitle">Lorem ipsum dolor sit amet. </p>
+                </Link>
             </div>
-        )
+        );
     }
 }
 
