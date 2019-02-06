11ab28de0fc40e3c56ac51fbdffe5b42a25c1c00
janaravi10 <janaravi962@gmail.com>
2019-02-06 17:39:51 +0530

    Added a simple api route for getting the list of posts called Wiiti

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.get("/", (req, res) => {
-    res.send("hello world jana");
+const cors = require("cors");
+const corsOption = {
+    origin: "http://localhost:3000"
+}
+app.use(cors(corsOption));
+
+app.use(bodyParser.json());
+app.use("/images", express.static("images"));
+
+/*
+ * Route for getting all posts
+ */
+app.get("/api/wiitis", (req, res) => {
+    res.send([{ title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
+    { title: "waht is this image", imgSrc: "images/wiiti-image.jpg" },
+    { title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
+    { title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
+    { title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" }]);
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
