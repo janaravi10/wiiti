24d21206f4eaa7ea07852800d09104e6f62d71a1
janaravi10 <janaravi962@gmail.com>
2019-02-08 07:57:33 +0530

    Added Login and signup route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Header from "./components/Header";
 import Wiitis from "./components/Wiitis";
-import './App.css';
-
+import Post from "./components/Post";
+import "./App.css";
+import { Route, BrowserRouter as router } from "react-router-dom";
+import Login from "./components/Login";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header></Header>
-        <Wiitis></Wiitis>
+        <Header />
+        <Route exact path="/" component={Wiitis} />
+        <Route path="/post/:id" component={Post} /> 
+        <Route path="/login" render={props => <Login reason="LOGIN" {...props}/>} />
+        <Route path="/signup" render={props => <Login reason="SIGNUP" {...props} />} />
       </div>
     );
   }
