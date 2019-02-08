a77b89764d41dbad3980eda5dc84553988127fa1
janaravi10 <janaravi962@gmail.com>
2019-02-08 07:58:38 +0530

    added a temporary api response for route api/wiitis

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const corsOption = {
-    origin: "http://localhost:3000"
-}
+  origin: "http://localhost:3000"
+};
 app.use(cors(corsOption));
 
 app.use(bodyParser.json());
@@ -14,11 +14,17 @@ app.use("/images", express.static("images"));
  * Route for getting all posts
  */
 app.get("/api/wiitis", (req, res) => {
-    res.send([{ title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
-    { title: "waht is this image", imgSrc: "images/wiiti-image.jpg" },
-    { title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
-    { title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
-    { title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" }]);
+  res.send([
+    { postId: 1, title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
+    {
+      postId: 2,
+      title: "waht is this image",
+      imgSrc: "images/wiiti-image.jpg"
+    },
+    { postId: 3, title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
+    { postId: 4, title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" },
+    { postId: 5, title: "lorem ipsum kdls", imgSrc: "images/wiiti-image.jpg" }
+  ]);
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
