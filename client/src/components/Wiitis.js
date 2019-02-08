9bfd7f70a8c94b3faaf51da3fade5091f8d01972
janaravi10 <janaravi962@gmail.com>
2019-02-08 07:52:57 +0530

    Fetching the post datas through axios fetch

--- client/src/components/Wiitis.js
+++ client/src/components/Wiitis.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 import Wiiti from "./Wiiti";
+import axios from "axios";
 class Wiitis extends Component {
+    state = {
+        postList: []
+    }
+    componentDidMount() {
+        axios.get("http://localhost:5000/api/wiitis")
+            .then(res => {
+          
+                if (res.data.length) {
+                    this.setState({ postList: res.data });
+                }else{
+                    console.log("nothing")
+                }
+            });
+    }
     render() {
         return (
             <div className="wrapper">
-                <Wiiti></Wiiti>
-                <Wiiti></Wiiti>
-                <Wiiti></Wiiti>
-                <Wiiti></Wiiti>
-                <Wiiti></Wiiti>
-                <Wiiti></Wiiti>
+                {this.state.postList.map(post => {
+                    console.log(post);
+                    return <Wiiti postId={post.postId} imgSrc={post.imgSrc} title={post.title}></Wiiti>
+                })}
             </div>
         )
     }
