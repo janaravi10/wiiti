89b6b5f9074a7501f69301bb5d0690f4d2ca016c
janaravi10 <janaravi962@gmail.com>
2019-02-09 19:00:01 +0530

    added the session activity

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const cors = require("cors");
-const corsOption = {
-  origin: "http://localhost:3000"
-}; 
+const express = require("express"),
+  session = require("express-session"),
+  bodyParser = require("body-parser"),
+  app = express(),
+  cors = require("cors"),
+  corsOption = {
+    origin: "http://localhost:3000"
+  },
+  cookieParser = require("cookie-parser"),
+  port = process.env.PORT || 5000,
+  secret = process.env.PRIVATE_KEY_NODE;
+app.use(cookieParser());
+app.use(session({ secret, resave: false, saveUninitialized: false }));
 app.use(cors(corsOption));
 app.use(bodyParser.json());
 app.use("/images", express.static("images"));
-require("./routes/userRoute")(app)
+require("./routes/userRoute")(app);
 require("./routes/postRoute")(app);
-
-const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
