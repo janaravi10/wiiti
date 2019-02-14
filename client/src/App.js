d8b711a8fd3f3d990ab86cade1555d670bb1949b
janaravi10 <janaravi962@gmail.com>
2019-02-14 21:58:09 +0530

    connected the app component with redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,48 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { authSessAction } from "./actions/loginAction";
+import { withRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Wiitis from "./components/Wiitis";
 import Post from "./components/Post";
 import "./App.css";
-import { Route, BrowserRouter as router } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Login from "./components/Login";
 class App extends Component {
+  componentDidMount() {
+    this.props.authUser();
+    console.log(this.props.loggedIn);
+  }
   render() {
     return (
       <div className="App">
         <Header />
         <Route exact path="/" component={Wiitis} />
-        <Route path="/post/:id" component={Post} /> 
-        <Route path="/login" render={props => <Login reason="LOGIN" {...props}/>} />
-        <Route path="/signup" render={props => <Login reason="SIGNUP" {...props} />} />
+        <Route path="/post/:id" component={Post} />
+        <Route
+          path="/login"
+          render={props => <Login reason="LOGIN" {...props} />}
+        />
+        <Route
+          path="/signup"
+          render={props => <Login reason="SIGNUP" {...props} />}
+        />
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.user.loggedIn
+  };
+};
+const mapDispatchToProps = {
+  authUser: authSessAction
+};
 
-export default App;
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
