bfc802aff2967805bea7241c48bef54a54438515
janaravi10 <janaravi962@gmail.com>
2019-02-14 22:06:39 +0530

    added constant variable for auth session , sign_up_failed , getting email , loggout

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -2,3 +2,7 @@ export const LOGGED_IN = "LOGGED_IN";
 export const LOG_IN_FAILED = "LOG_IN_FAILED";
 export const LOG_OUT = "LOG_OUT";
 export const SIGN_UP = "SIGN_UP";
+export const AUTH_SESSION = "AUTH_SESSION";
+export const SIGN_UP_FAILED = "SIGN_UP_FAILED";
+export const GET_EMAIL_FROM_STORAGE = "GET_EMAIL_FROM_STORAGE";
+export const LOGGED_OUT = "LOGGED_OUT";
