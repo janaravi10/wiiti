5416fac2f0a7e8aa3757bbf84f4a59cbea2973fe
janaravi10 <janaravi962@gmail.com>
2019-02-14 21:59:29 +0530

    adde the condition in switch statement for all sessionauth , logut , login , get_email_from_storage

--- client/src/reducers/loginReducer.js
+++ client/src/reducers/loginReducer.js
@@ -1,15 +1,27 @@
-import { LOGGED_IN } from "../actions/actionTypes";
-export default function(
-  initialState = {
-    user: {
-      loggedIn: false
-    }
-  },
-  action
-) {
+import {
+  LOGGED_IN,
+  AUTH_SESSION,
+  LOG_IN_FAILED,
+  SIGN_UP,
+  GET_EMAIL_FROM_STORAGE,
+  LOG_OUT,
+  LOGGED_OUT
+} from "../actions/actionTypes";
+export function logIn(initialState = { loggedIn: false }, action) {
   switch (action.type) {
     case LOGGED_IN:
-    console.log("helo");
+      return Object.assign({}, initialState, action.payload);
+    case AUTH_SESSION:
+      return Object.assign({}, initialState, action.payload);
+    case LOG_IN_FAILED:
+      return Object.assign({}, initialState, action.payload);
+    case SIGN_UP:
+      return Object.assign({}, initialState, action.payload);
+    case GET_EMAIL_FROM_STORAGE:
+      return Object.assign({}, initialState, action.payload);
+    case LOG_OUT:
+      return Object.assign({}, initialState, action.payload);
+    case LOGGED_OUT:
       return Object.assign({}, initialState, action.payload);
     default:
       return initialState;
