d21e72b0cc80906025c8f24e9084eabe33ea2484
janaravi10 <janaravi962@gmail.com>
2019-02-14 22:05:04 +0530

    Added the profile user name and logout button on the top right corner and connect the redux store with this component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,63 @@
-import React, { Component } from 'react';
-import '../css/header.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "../css/header.css";
 import brandLogo from "../img/logoBlue.png";
+import profileLogo from "../img/profileLogo.png";
+import { Link } from "react-router-dom";
+import { getEmail, logoutAction } from "../actions/loginAction";
+import { createBrowserHistory } from "history";
 
 class Header extends Component {
+  componentDidMount() {
+    // getting the email from storage
+    this.props.getEmail();
+  }
+  logout = () => {
+    const history = createBrowserHistory();
+    this.props.logoutUser(history);
+  };
   render() {
     return (
       <header>
-        <img className="brand-logo" src={brandLogo} alt="Brand logo" />
+        <div className="brand-container">
+          <img className="brand-logo" src={brandLogo} alt="Brand logo" />
+        </div>
+        <div className="header-profile">
+          {this.props.loggedIn ? (
+            <img src={profileLogo} className="profile-logo" alt="" />
+          ) : (
+            <Link to="/login" className="loginBtn">
+              Login
+            </Link>
+          )}
+          {this.props.loggedIn ? (
+            <ul className="dropdown-user">
+              <li>{this.props.email}</li>
+              <li>
+                <span className="logout" onClick={this.logout}>
+                  Logout
+                </span>
+              </li>
+            </ul>
+          ) : (
+            ""
+          )}
+        </div>
       </header>
-    )
+    );
   }
 }
-
-export default Header;
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.user.loggedIn,
+    email: state.user.email
+  };
+};
+const mapDispatchToProps = {
+  getEmail,
+  logoutUser: logoutAction
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
