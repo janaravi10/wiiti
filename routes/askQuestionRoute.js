397d4c10f44f07fdedf44e7b293c7a4e4d095375
janaravi10 <janaravi962@gmail.com>
2019-02-16 19:14:26 +0530

    Seperate route for handling new questions

---
+++ routes/askQuestionRoute.js
@@ -0,0 +1,115 @@
+let multer = require("multer"),
+  path = require("path"),
+  fs = require("fs"),
+  questionModel = require("../models/questionModel"),
+  baseUploadLocation = "http://www.localhost:5000/uploads/",
+  verifyToken = require("../util/jwtVerify");
+module.exports = app => {
+  /*
+   * Storage area
+   */
+  let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      // check if uploads directory is available
+      if (fs.existsSync("./uploads/")) {
+        cb(null, "./uploads/");
+      } else {
+        // create the folder and upload the images in it
+        fs.mkdirSync("./uploads");
+        cb(null, "./uploads/");
+      }
+    },
+    filename: function(req, file, cb) {
+      // function to create file name for the file uploading
+      cb(
+        null,
+        file.originalname.split(".")[0].replace(/\s+/g, "") +
+          "-" +
+          Date.now() +
+          path.extname(file.originalname)
+      );
+    }
+  });
+  // handle file uploading
+  function handleFileUpload(req, res, next) {
+    upload(req, res, function(err) {
+      if (!req.files.length) {
+        // check if atleast one image is provided
+        return res.json({
+          success: false,
+          error: "Please provide atleast one image!"
+        });
+      }
+      if (!req.body.questionTitle && req.body.questionTitle.length < 10) {
+        // check if atleast one image is provided
+        return res.json({
+          success: false,
+          error: "Please provide question!"
+        });
+      }
+      if (err) {
+        // err.code is multer set value
+        if (err.code === "LIMIT_FILE_SIZE") {
+          return res.json({ success: false, error: err.message });
+        } else if (err.message === "INVALID_FILE_TYPE") {
+          // err.message is own value from the file filter function
+          return res.json({ success: false, error: err.message });
+        } else {
+          return res.json({ success: false, error: err.message });
+        }
+      } else {
+        next();
+      }
+    });
+  }
+
+  // file filter function
+  function fileFilter(req, file, cb) {
+    let imgMime = ["image/jpeg", "image/png"];
+    if (imgMime.indexOf(file.mimetype) === -1) {
+      return cb(new Error("INVALID_FILE_TYPE"), false);
+    }
+    cb(null, true);
+  }
+  // start the upload
+  let upload = multer({
+    storage: storage,
+    fileFilter,
+    limits: { fileSize: 1024 * 1024 }
+  }).array("file[]");
+
+  /*
+   * route for api/new question
+   */
+
+  app.post("/api/new-question", [verifyToken, handleFileUpload], function(
+    req,
+    res,
+    next
+  ) {
+    let questionData = {
+        questionTitle: req.body.questionTitle,
+        imgUrls: [],
+        authorId: req.userId
+      },
+      questionDoc;
+    // looping through the uploaded files and finding the filenames
+    req.files.forEach(file => {
+      questionData.imgUrls.push(baseUploadLocation + file.filename);
+    });
+    questionDoc = new questionModel(questionData);
+    // saving the data to the mongodb database;
+    questionDoc
+      .save()
+      .then(function(savedQuestion) {
+        // sending the response
+        res.send({
+          success: true,
+          error: "Question uploaded Successfully!",
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+};
