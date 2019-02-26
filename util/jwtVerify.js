a81b9c26c42e228c3a6ccf57abcc93c8beeb44fa
janaravi10 <janaravi962@gmail.com>
2019-02-26 18:39:24 +0530

    implemented mysql jwt verification

--- util/jwtVerify.js
+++ util/jwtVerify.js
@@ -1,6 +1,6 @@
 let jwt = require("jsonwebtoken"),
-  logoutModel = require("../models/logoutModel"),
-  secret = process.env.PRIVATE_KEY_NODE;
+  secret = process.env.PRIVATE_KEY_NODE,
+  db = require("../db_connection/connection");
 module.exports = function verifyToken(req, res, next) {
   // getting the token header
   const bearer = req.headers.authorization;
@@ -8,23 +8,24 @@ module.exports = function verifyToken(req, res, next) {
     const token = bearer.split(" ")[1];
     // finding whether this token is stored in the logout collection
     // if it is stored then this is already logged out token
-    logoutModel.findOne({ token }, function(err, doc) {
+    let sql = "SELECT * FROM tokens WHERE ?";
+    db.query(sql, { token }, function(err, doc) {
       if (err) {
         res.json({ success: false, error: "Internal server error" });
         return;
       }
-      if (doc) {
+      if (doc.length) {
         jwt.verify(token, secret, function(err, decoded) {
           if (err) {
             // jwt verification failed then update the isExpired value to true
-            doc.isExpired = true;
-            doc.save(function(err) {
+            db.query("UPDATE tokens SET ?", { isExpired: true }, function(doc,err) {
               if (err) {
                 console.error("ERROR!");
               }
             });
           }
-          // send token expired header because user logged this jwt token
+          console.log("giving error here drer");
+          // send token expired header because user logged out this jwt token
           res.json({
             success: false,
             statusCode: 401,
@@ -35,11 +36,13 @@ module.exports = function verifyToken(req, res, next) {
         // jwt verify this token
         jwt.verify(token, secret, function(err, decoded) {
           if (err) {
+            console.log("giving error here");
             res.json({
               success: false,
               statusCode: 401,
               error: "Token Expired!"
             });
+            console.log(err);
           } else {
             console.log(decoded);
             // setting the user id;
@@ -51,6 +54,7 @@ module.exports = function verifyToken(req, res, next) {
       }
     });
   } else {
+
     res.json({
       success: false,
       statusCode: 401,
