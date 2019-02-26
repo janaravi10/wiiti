14be3f62bfb8bf841c006b1a6a6c42ec0dfe1310
janaravi10 <janaravi962@gmail.com>
2019-02-26 18:36:56 +0530

    added answerRoute for handling the question answer request

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const express = require("express"),
     credentials: true
   },
   cookieParser = require("cookie-parser"),
-  port = process.env.PORT || 5000;
+  port = process.env.PORT || 5000,
+  db = require("./db_connection/connection");
 app.use(cookieParser());
 app.use(cors(corsOption));
 app.use(bodyParser.json());
 app.use("/images", express.static("images"));
 app.use("/uploads", express.static("uploads"));
-require("./routes/userRoute")(app);
-require("./routes/postRoute")(app);
-require("./routes/askQuestionRoute")(app);
+require("./routes/userRoute")(app, db);
+require("./routes/postRoute")(app, db);
+require("./routes/askQuestionRoute")(app, db);
+require("./routes/answerRoute")(app, db);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
